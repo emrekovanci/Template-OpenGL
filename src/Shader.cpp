a33225bddf7016c23899ba443ae66ec911aaffae
Emre Kovanci <kovanciemre@gmail.com>
2023-08-03 22:11:23 +0300

    Update Shader.cpp

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -35,11 +35,11 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
 {
     std::string vertexCode;
     std::string fragmentCode;
-    std::ifstream vShaderFile;
-    std::ifstream fShaderFile;
 
-    // ensure ifstream objects can throw exceptions:
+    std::ifstream vShaderFile;
     vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+    std::ifstream fShaderFile;
     fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
 
     try
@@ -58,16 +58,15 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
         std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ: " << e.what() << std::endl;
     }
 
-    const char* vShaderCode = vertexCode.c_str();
-    const char* fShaderCode = fragmentCode.c_str();
-
     // vertex shader
+    const char* vShaderCode = vertexCode.c_str();
     unsigned int vertex = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex, 1, &vShaderCode, nullptr);
     glCompileShader(vertex);
     checkCompileErrors(vertex, "VERTEX");
 
     // fragment Shader
+    const char* fShaderCode = fragmentCode.c_str();
     unsigned int fragment = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragment, 1, &fShaderCode, nullptr);
     glCompileShader(fragment);
@@ -80,7 +79,7 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
     glLinkProgram(_id);
     checkCompileErrors(_id, "PROGRAM");
 
-    // delete the shaders as they're linked into our program now and no longer necessary
+    // they're linked into our program now and no longer necessary
     glDeleteShader(vertex);
     glDeleteShader(fragment);
 }
